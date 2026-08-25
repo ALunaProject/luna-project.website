81c2be96467c985ba0981b1f8b895b483bb67772
Lyan Brito <lyanbrito613@gmail.com>
2026-08-25 10:07:39 -0300

    [STYLES] Created Global Styles (#46)
    * chore: ignored generated file
    
    * styles: created global styles
    
    * refactor: updated global style in app
    
    * fix: test/dev home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,37 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono, Poppins } from "next/font/google";
-import "./globals.css";
+import type {Metadata} from "next";
+import {Geist, Geist_Mono, Poppins} from "next/font/google";
+import "@/styles/main.scss"
 
 const PoppinsFont = Poppins({
-  adjustFontFallback: false,
-  display: "swap",
-  fallback: [],
-  preload: true,
-  subsets: ["latin"],
-  variable: "--font-poppins",
-  weight: ["400", "500"],
-})
+    weight: ["100","200","300","400","500","600","700","800","900"],
+    subsets: ["latin"],
+    variable: "--font-poppins",
+    preload: true,
+    display: "swap",
+});
 
 export const metadata: Metadata = {
-  title: "Luna | Your game place",
-  description: "placeholder",
+    title: "Luna | Your game place",
+    description: "placeholder",
 };
 
-export default function RootLayout({ children }: LayoutProps<"/">) {
-  return (
-      <html lang="en" className={`${PoppinsFont.variable} ${PoppinsFont.variable}`}>
-      <body>{children}</body>
-      </html>
-  );
+export default function RootLayout({
+                                       children,
+                                   }: {
+    children: React.ReactNode;
+}) {
+    return (
+        <html lang="pt-br">
+        <head>
+            <meta
+                name="keywords"
+            />
+        </head>
+        <body
+            className={`${PoppinsFont.variable}`}
+        >
+        {children}
+        </body>
+        </html>
+    );
 }
