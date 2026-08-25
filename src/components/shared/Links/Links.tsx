a7f5fde1e517eb76b038219e9d45c32b00b45597
lyanbrito <lyanbrito613@gmail.com>
2026-08-25 11:09:17 -0300

    feat: created basic link compoent

---
+++ src/components/shared/Links/Links.tsx
@@ -0,0 +1,19 @@
+import * as I from "@/assets/icons/Icons";
+import Link from "next/link";
+import s from "./styles.module.scss"
+
+interface LinkProps {
+    href: string;
+    label: string;
+    icon: keyof typeof I;
+}
+
+export default function Links({href, label, icon}: LinkProps) {
+const Icon = I[icon]
+    return (
+        <Link className={s.linkContainer} href={href}>
+            <Icon />
+            {label}
+        </Link>
+    )
+}
