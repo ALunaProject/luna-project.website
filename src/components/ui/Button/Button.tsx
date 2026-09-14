ae61ca6df606ed33a890b2453fc2fab4e559cf4e
lyanbrito <lyanbrito613@gmail.com>
2026-09-13 23:13:01 -0300

    lint: biome format

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -3,17 +3,17 @@ import * as I from "@/assets/icons/LinksIcons"
 import s from "./styles.module.scss"
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-    label: string
-    icon?: keyof typeof I
+	label: string
+	icon?: keyof typeof I
 }
 
 export default function Button({ label, icon, ...rest }: ButtonProps) {
-    const Icon = icon ? I[icon] : null
+	const Icon = icon ? I[icon] : null
 
-    return (
-        <button className={s.buttonContainer} {...rest}>
-            {Icon ? <Icon /> : null}
-            <span>{label}</span>
-        </button>
-    )
+	return (
+		<button className={s.buttonContainer} {...rest}>
+			{Icon ? <Icon /> : null}
+			<span>{label}</span>
+		</button>
+	)
 }
